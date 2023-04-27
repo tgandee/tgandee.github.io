362596cc25f1e3335ac4572ffa86936f6b9b898a
gandeetj <gandeetj@miamioh.edu>
2023-04-27 10:44:07 -0400

    Added GitHub website navigation

--- src/components/Navbar/consts/navbarItems.js
+++ src/components/Navbar/consts/navbarItems.js
@@ -9,17 +9,24 @@ import AboutMe from "../../../pages/AboutMe/AboutMe";
 import Authentication from "../../../pages/Authentication/Authentication";
 import MachineLearning from "../../../pages/MachineLearning/MachineLearning";
 import Hosting from "../../../pages/Hosting/Hosting";
+import Home from "../../../pages/Home/Home";
 import Functions from "../../../pages/Functions/Functions";
 import Database from "../../../pages/Database/Database";
 import Storage from "../../../pages/Storage/Storage";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Link, useLocation } from "react-router-dom";
-import { redirect } from "react-router-dom";
-import { useState, useNavigate } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 var i = 0;
 
 export const mainNavbarItems = [
+  {
+    id: i++,
+    icon: <HomeIcon />,
+    element: <Home />,
+    label: "Home",
+    route: "/",
+  },
   {
     id: i++,
     icon: <PersonIcon />,
@@ -32,6 +39,12 @@ export const mainNavbarItems = [
     icon: <LinkedInIcon />,
     label: "LinkedIn",
     route: "linkedin",
+  },
+  {
+    id: i++,
+    icon: <GitHubIcon />,
+    label: "GitHub",
+    route: "github",
   },
   {
     id: i++,
