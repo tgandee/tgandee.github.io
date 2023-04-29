aa2c062da0ffbf372ad050f53c7b22e9145b1362
gandeetj <61200082+gandeetj@users.noreply.github.com>
2023-04-29 10:44:22 -0400

    Fixed deployment refresh error

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { mainNavbarItems } from "./components/Navbar/consts/navbarItems";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         {mainNavbarItems.map((item, index) => (
@@ -16,7 +16,7 @@ root.render(
         ))}
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
